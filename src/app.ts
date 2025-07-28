f94768fe49dc543fc424afd019af3e7b422cc2b8
Ahmad Musa Abdullah <ahmadmusa9805@gmail.com>
2025-07-28 11:18:25 +0600

    add some code

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
-import notFound from './app/middlewares/notFound';
+import notFound from './app/middlewares/notFound.js';
 import router from './app/routes';
 import { createServer } from 'http';
 import helmet from 'helmet';
